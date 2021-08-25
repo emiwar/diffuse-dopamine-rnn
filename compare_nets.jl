5b76481ba4963d906265ba56fee8246627dbd638
Emil Wärnberg <emilwa@kth.se>
2021-08-25 11:58:12 +0200

    Added a simple model of dopamine feedback, and made a comparison between this and ideal feedback, no dopamine and an odd model that feeds back the loss as a scalar rather than as a vector.

---
+++ compare_nets.jl
@@ -0,0 +1,37 @@
+using Plots
+include("dale_net.jl")
+include("rpe_net.jl")
+include("dopamine_net.jl")
+
+t_max = 100
+theta = range(0,2pi;length=t_max)
+y_target = @. sin(theta)+0.5sin(2theta)+0.25sin(4theta)
+inp =[sin.(theta) cos.(theta) -sin.(theta) -cos.(theta)] .+ 1
+
+nets = [RPE_NET(4, 100, 1; eta_in=1e-3, eta_rec=1e-3, eta_out=1e-3),
+        DOPAMINE_NET(4, 100, 1; eta_in=0.0, eta_rec=0.0, eta_out=1e-3),
+        DOPAMINE_NET(4, 100, 1; eta_in=1e-3, eta_rec=1e-3, eta_out=1e-3),
+        DALE_NET(4, 100, 1; eta_in=1e-3, eta_rec=1e-3, eta_out=1e-3)]
+
+losses = [Float64[] for _=1:4]
+n_episodes = 1200
+for i=1:4
+    for ep=ProgressBar(1:n_episodes)
+        y, h = episode!(nets[i], inp, y_target)
+        push!(losses[i], sum((y.-y_target).^2)/t_max)
+    end
+    
+end
+labels = ["Scalar dopamine" "No dopamine" "Vector dopamine" "Ideal (non-local)"]
+l = @layout [a{0.6w} [b;c;d;e]]
+p = plot(layout=l)
+plot!(p[1], losses, xlim=(1, n_episodes), ylim=(1e-4, 1e0),
+     xticks=[1,div(n_episodes, 2),n_episodes], yaxis=:log,
+     labels=labels, xlabel="Episode", ylabel="Squared loss")
+for i=1:4
+    plot!(p[i+1], y_target, legend=false, linestyle=:dash, color=:black)
+    y, h = episode!(nets[i], inp, y_target)
+    plot!(p[i+1], y, color=i)
+end
+plot!(p[2], title="Final output")
+plot!(p[5], xlabel="Time")
---
+++ dopamine_net.jl
@@ -0,0 +1,93 @@
+using LinearAlgebra
+
+mutable struct DOPAMINE_NET
+    W_in::Matrix{Float64}
+    W_rec::Matrix{Float64}
+    W_out::Matrix{Float64}
+    DA::Matrix{Float64}
+    tau_m::Float64
+    eta_in::Float64
+    eta_rec::Float64
+    eta_out::Float64
+    v::Vector{Float64}
+    s::Vector{Float64}
+    p::Matrix{Float64}
+    q::Matrix{Float64}
+end
+
+function DOPAMINE_NET(W_in, W_rec, W_out, DA, tau_m, eta_in, eta_rec, eta_out)
+    n_hidden, n_in = size(W_in)
+    DOPAMINE_NET(W_in, W_rec, W_out, DA, tau_m, eta_in, eta_rec, eta_out,
+             zeros(n_hidden), zeros(n_hidden), zero(W_rec), zero(W_in))
+end
+
+function DOPAMINE_NET(W_in, W_rec, W_out, DA, tau_m, eta)
+    DOPAMINE_NET(W_in, W_rec, W_out, DA, tau_m, eta, eta, eta)
+end
+
+function DOPAMINE_NET(n_in, n_hidden, n_out; in_scale=0.8, rec_scale=-2.0,
+                      out_scale=2.0, tau_m=10, eta_in=5e-3, eta_rec=5e-3,
+                      eta_out=5e-3, lambda=0.1)
+    W_in = in_scale*rand(n_hidden, n_in)
+    W_rec = rec_scale*rand(n_hidden, n_hidden) / n_hidden
+    W_out = out_scale*(2*rand(n_out, n_hidden) .- 1) / sqrt(n_hidden)
+    DA = draw_DA(n_hidden, n_out; lambda=lambda)
+    DA[end÷2+1:end, :] .*= -1
+    DOPAMINE_NET(W_in, W_rec, W_out, DA, tau_m, eta_in, eta_rec, eta_out)
+end
+function reset_state!(net::DOPAMINE_NET)
+    net.v .= 0.0
+    net.p .= 0.0
+    net.q .= 0.0
+    
+end
+function step!(net::DOPAMINE_NET, inp, target)
+    prev_h = @. 1/(1+exp(-net.v))
+    u = net.W_rec*prev_h + net.W_in*inp .- 2
+    net.v += -net.v/net.tau_m + u
+    #net.s += (1 .- net.s .- prev_h.*prev_h)/(0.5*net.tau_m)
+    
+    h = @. 1/(1+exp(-net.v))
+    dh = @. (1-h)*h
+    net.p += -net.p/net.tau_m + dh*prev_h'
+    net.q += -net.q/net.tau_m + dh*inp'
+    
+    y = net.W_out*h
+    err = target-y
+    feedback = net.DA*err
+    
+    net.W_out += net.eta_out*   err   *h'
+    net.W_rec += net.eta_rec*feedback.*net.p
+    net.W_in  += net.eta_in *feedback.*net.q
+    
+    net.W_rec .= min.(net.W_rec, 0)
+    net.W_in .= max.(net.W_in, 0)
+    net.W_out[:, 1:end÷2] .= max.(net.W_out[:, 1:end÷2], 0)
+    net.W_out[:, end÷2+1:end] .= min.(net.W_out[:, end÷2+1:end], 0)
+    return y
+end
+
+function episode!(net::DOPAMINE_NET, inp, target)
+    reset_state!(net)
+    t_max = size(inp)[1]
+    y = zero(target)
+    h = zeros(t_max, length(net.v))
+    for t=2:t_max
+        y[t, :] = step!(net, inp[t, :], target[t, :])
+        h[t, :] = @. 1/(1+exp(-net.v))
+    end
+    return y, h
+end
+
+function draw_DA(n_hidden, n_outputs; n_varicosities=10, lambda=0.1)
+    DA = zeros(n_hidden, n_outputs)
+    hidden_pos = rand(n_hidden, 3)
+    for i=1:n_outputs
+        varicosities_pos = rand(n_varicosities, 3)
+        for j=1:n_hidden, k=1:n_varicosities
+            d = norm(hidden_pos[j, :] - varicosities_pos[k, :])
+            DA[j, i] += exp(-d/lambda)
+        end
+    end
+    return DA
+end
